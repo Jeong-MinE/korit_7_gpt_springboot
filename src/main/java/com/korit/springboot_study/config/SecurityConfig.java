1999db39e300e769ebf393b65b4ee50f5d5e5bab
Jeong-MinE <Jeong-MinE>
2025-02-17 17:35:00 +0900

    security signup, in

--- src/main/java/com/korit/springboot_study/config/SecurityConfig.java
+++ src/main/java/com/korit/springboot_study/config/SecurityConfig.java
@@ -1,13 +1,13 @@
 package com.korit.springboot_study.config;
 
 import com.korit.springboot_study.security.exception.CustomAuthenticationEntryPoint;
-import com.korit.springboot_study.security.filter.CustomAuthenticationFilter;
 import com.korit.springboot_study.security.filter.JwtAuthenticationFilter;
 import com.korit.springboot_study.security.oauth2.OAuth2Service;
 import com.korit.springboot_study.security.oauth2.OAuth2SuccessHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -38,37 +38,37 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.cors();
         http.csrf().disable();
         http.httpBasic().disable();
         http.formLogin().disable();
         http.sessionManagement()
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
         http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
-        http.exceptionHandling().authenticationEntryPoint(customAuthenticationEntryPoint);
+        http.exceptionHandling()
+                .authenticationEntryPoint(customAuthenticationEntryPoint);
 
         http.oauth2Login()
                 .successHandler(oAuth2SuccessHandler)
-                        .userInfoEndpoint()
-                                .userService(oAuth2Service);
+                .userInfoEndpoint()
+                .userService(oAuth2Service);
 
         http.authorizeRequests()
                 .antMatchers(
                         "/swagger-ui/**",
                         "/v2/api-docs/**",
                         "/v3/api-docs/**",
-                        "/swagger-resources/**"
+                        "/swagger-resources/**",
+                        "/server/hc"
                 )
                 .permitAll()
-
                 .antMatchers("/api/auth/**")
                 .permitAll()
-
+                .antMatchers(HttpMethod.GET, "/api/post/**")
+                .permitAll()
                 .anyRequest()
                 .authenticated();
-//                .and()
-//                .exceptionHandling()
-//                .authenticationEntryPoint(customAuthenticationEntryPoint);
     }
 }
